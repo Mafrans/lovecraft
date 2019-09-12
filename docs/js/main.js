eb7890f996352d5048cdbb1efa81f22aa03766b3
BuildTools <unconfigured@null.spigotmc.org>
2019-09-12 16:26:44 +0200

    Redesign and Sidebar

---
+++ docs/js/main.js
@@ -0,0 +1,46 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector(".sidebar");
+
+    if(sidebar.classList.contains("collapse")) {
+        sidebar.classList.remove("collapse");
+    }
+    else {
+        sidebar.classList.add("collapse");
+    }
+}
+window.onload = function(e) {
+    var anchors = document.getElementsByClassName("ripple");
+    console.log(anchors.length);
+
+    for(let a of anchors) {
+        //var a = anchors[i];
+        
+        a.addEventListener("mousedown", () => {
+            console.log("click");
+        
+            a.classList.add("click");
+            setTimeout(()=>{
+                a.classList.remove("click");
+                a.classList.add("clicking");
+                setTimeout(()=>{
+                    a.classList.remove("clicking");
+                }, 500)
+            }, 100);
+            this.event.stopPropagation();
+        })
+    }
+
+
+    var sidebar = document.querySelector(".sidebar");
+
+    window.addEventListener("mousedown", (event) => {
+        this.console.log("clc");
+        if(!sidebar.classList.contains("collapse")) {
+            sidebar.classList.add("collapse");
+        }
+    });
+
+    sidebar.addEventListener("mousedown", (event) => {
+        event.stopPropagation();
+    }); 
+}   
